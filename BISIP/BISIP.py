b6d2cc36536212bc88a5b9425d883978b73f1a94
clberube <charleslberube@gmail.com>
2017-09-29 16:32:00 -0400

    adding stuff

---
+++ BISIP/BISIP.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 29 16:15:47 2017
+
+@author: Charles
+"""
+
+import BISIP_GUI
+import BISIP_models
+import BISIP_invResults
+import BISIP_cython_funcs
+from BISIP_models import mcmcSIPinv
+
+
+
+
---
+++ BISIP/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 29 16:18:50 2017
+
+@author: Charles
+"""
+
+from setuptools import setup
+
+setup(
+    name='BISIP',      # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['BISIP']                  # The name of your scipt, and also the command you'll be using for calling it
+)
+
